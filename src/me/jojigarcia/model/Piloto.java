c22d8a861402acceea42f81ab735a7ec198939e3
joaquinjimenezgarcia <garcia.jjimenez@gmail.com>
2017-05-09 15:43:25 +0200

    - Creada clase piloto

--- src/me/jojigarcia/model/Piloto.java
+++ src/me/jojigarcia/model/Piloto.java
@@ -9,4 +9,63 @@ public class Piloto {
     private Nacionalidad nacionalidad;
     private Escuderia escudería;
     private Tipo tipo;
+
+    public Piloto(int dorsal, String nombre, Nacionalidad nacionalidad, Escuderia escudería, Tipo tipo) {
+        this.dorsal = dorsal;
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.escudería = escudería;
+        this.tipo = tipo;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Nacionalidad getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(Nacionalidad nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public Escuderia getEscudería() {
+        return escudería;
+    }
+
+    public void setEscudería(Escuderia escudería) {
+        this.escudería = escudería;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Piloto{" +
+                "dorsal=" + dorsal +
+                ", nombre='" + nombre + '\'' +
+                ", nacionalidad=" + nacionalidad +
+                ", escudería=" + escudería +
+                ", tipo=" + tipo +
+                '}';
+    }
 }
